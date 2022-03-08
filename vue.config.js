f0c77f01c4866186b2c5a08eaee3abaa895f14e7
Ceri Binding <ceri.binding@southwales.ac.uk>
2022-03-08 11:51:10 +0000

    comments removed

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,8 @@
 module.exports = {
-  // changed 08/03/2021 @ 11:36...
   crossorigin: "anonymous",
   publicPath: process.env.NODE_ENV === 'production'
     ? '/tmp123/' // for github.io deployment
     : '/',
   productionSourceMap: process.env.NODE_ENV !== 'production', //https://stackoverflow.com/questions/51482940/how-can-i-disable-source-maps-in-production-for-a-vue-js-app
-    /*chainWebpack: config => {
-      //config.resolve.alias.set('vue', '@vue/compat')
-  
-      config.module
-        .rule('vue')
-        .use('vue-loader')
-        .tap(options => {
-          return {
-            ...options,
-            compilerOptions: {
-              compatConfig: {
-                MODE: 2
-              }
-            }
-          }
-        })
-    }*/
-    lintOnSave: false
+  lintOnSave: false
 } // vue.config.js
